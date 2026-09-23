bca9d079b98697fa1834f2ab50e22f15a5916679
Chris Wilson <divclass01@gmail.com>
2026-09-22 20:47:52 -0400

    Resolve page v2 data in I-01 smoke test

--- scripts/smoke_test_i01.py
+++ scripts/smoke_test_i01.py
@@ -16,6 +16,7 @@ sys.path.insert(0, str(ROOT / "art_pipeline"))
 from comfy_cli_runner import ComfyCli, ComfyCliError
 from comfy_client import ComfyClient
 from prompt_builder import build_prompt
+from page_contract import resolve_page_spec
 from qa import inspect_candidate
 from flux2_klein_profile import envelope_data, prepare_distilled_text_to_image
 
@@ -52,7 +53,7 @@ def get_current():
     state = read_json(STATE_FILE)
     page_id = state["current_page_id"]
     page = next(p for p in tome["pages"] if p["page_id"] == page_id)
-    return page, state["pages"][page_id]
+    return resolve_page_spec(page, ROOT), state["pages"][page_id]
 
 
 def model_filename(config: dict, folder: str) -> str:
